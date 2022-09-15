ffc43720015f4cfa9d09ab386f324b6f0a78485b
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-09-15 23:19:08 +0600

    layout implement in project

---
+++ resources/views/backend/dashboard/index.blade.php
@@ -0,0 +1,7 @@
+@extends('backend.layout.master')
+@section('title','Shikhi | Dashboard')
+@section('page','Dashboard')
+
+@section('content')
+
+@endsection
--- resources/views/backend/layout/master.blade.php
+++ resources/views/backend/layout/master.blade.php
@@ -12,25 +12,26 @@
 
         <div class="page-content">
             <div class="container-fluid">
-
-                @yield('page-content')
                 <!-- start page title -->
                 <div class="row">
                     <div class="col-12">
                         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                            <h4 class="mb-sm-0">Starter</h4>
 
                             <div class="page-title-right">
                                 <ol class="breadcrumb m-0">
                                     <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
-                                    <li class="breadcrumb-item active">Starter</li>
+                                    <li class="breadcrumb-item active">@yield('page')</li>
                                 </ol>
                             </div>
+
+                            <h4 class="mb-sm-0">@yield('page')</h4>
 
                         </div>
                     </div>
                 </div>
                 <!-- end page title -->
+
+                @yield('page-content')
 
             </div>
             <!-- container-fluid -->
