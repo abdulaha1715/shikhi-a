586f4b289e3f3fc150225c9eb1f1d3572dee8861
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-09-15 22:36:22 +0600

    user migration and demo user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +21,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name'              => 'Abdulaha Islam',
+            'username'          => 'abdulaha1715',
+            'email'             => 'abdulahaislam210917@gmail.com',
+            'password'          => bcrypt('01918786189'),
+            'email_verified_at' => now(),
+            'avatar'            => 'https://picsum.photos/300',
+        ]);
     }
 }
